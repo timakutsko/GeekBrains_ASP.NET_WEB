923db53ef0b1024f9dd9bece33dc08af098c2a8f
Tsimafei <tima.kutsko@gmail.com>
2022-08-21 21:36:25 +0300

    Lesson1
    Async write to file

---
+++ Lesson1/Lesson1/MyClient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    /// <summary>
+    /// Класс для осуществления запроса
+    /// </summary>
+    static class MyClient
+    {
+        private static readonly HttpClient _client = new HttpClient();
+
+        public static async Task<string> GetResponse(int id, CancellationTokenSource cts)
+        {
+            try
+            {
+                HttpResponseMessage response = await _client.GetAsync($"https://jsonplaceholder.typicode.com/posts/{id}", cts.Token);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                return responseBody;
+            }
+            catch (HttpRequestException ex)
+            {
+                return ($"Ошибка: {ex}.\nMessage :{ex.Message} ");
+            }
+        }
+    }
+}
---
+++ Lesson1/Lesson1/Response.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson1
+{
+    /// <summary>
+    /// Структура для сериализации JSON-ответа
+    /// </summary>
+    struct Response
+    {
+        public int userId { get; set; }
+
+        public int id { get; set; }
+
+        public string title { get; set; }
+
+        public string body { get; set; }
+
+    }
+}
